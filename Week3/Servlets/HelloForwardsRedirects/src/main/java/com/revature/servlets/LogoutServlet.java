bb1ce4c7f8bf6e8aae4a5ffdf454ea769c858b5d
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-03 17:33:56 -0400

    added redirects to HelloRedirects

--- Week3/Servlets/HelloForwardsRedirects/src/main/java/com/revature/servlets/LogoutServlet.java
+++ Week3/Servlets/HelloForwardsRedirects/src/main/java/com/revature/servlets/LogoutServlet.java
@@ -1,7 +1,25 @@
 package com.revature.servlets;
 
-public class LogoutServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutServlet extends HttpServlet {
 	
+	//we'll use a redirect here instead of a forward	
 	
+	//btw, what's IOException?
+	//IOException happens when there is a failure during reading, writing, and searching file or directory operations
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.sendRedirect(""); //here, we redirect to the homepage with ""
+												//aka the base URL.
+												//But you could really put any URL!
+		
+		//don't forget, all servlets must be declared and mapped in the web.xml.
+	}
+
 }
