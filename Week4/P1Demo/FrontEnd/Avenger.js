03fc5d1d65e566c5ad7fdd147d8cee7045280a35
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-07 18:53:28 -0400

    get all avengers works - still need to implement login

--- Week4/P1Demo/FrontEnd/Avenger.js
+++ Week4/P1Demo/FrontEnd/Avenger.js
@@ -32,21 +32,44 @@ async function assembleFunc() { //async returns a promise (which fetch returns)
             row.appendChild(cell) //this row now has the first cell (av_id)
 
             //the we'll do this^ for each field in the avenger model
+            let cell2 = document.createElement("td");
+            cell2.innerHTML = avenger.av_name;
+            row.appendChild(cell2);
+
+            let cell3 = document.createElement("td");
+            cell3.innerHTML = avenger.av_power;
+            row.appendChild(cell3);
+
+            let cell4 = document.createElement("td");
+            cell4.innerHTML = avenger.first_name;
+            row.appendChild(cell4);
+
+            let cell5 = document.createElement("td");
+            cell5.innerHTML = avenger.last_name;
+            row.appendChild(cell5);
+
+            let cell6 = document.createElement("td");
+            cell6.innerHTML = avenger.power_level;
+            row.appendChild(cell6);
+
+            //if the avenger has a home, just fill the cell with the home name
+            if(avenger.home_fk != null){
+                console.log("home came through")
+                let cell7 = document.createElement("td");
+                cell7.innerHTML = avenger.home_fk.homeName;
+                row.appendChild(cell7);
+            } else { //otherwise, still append the cell but leave it empty
+                let cell7 = document.createElement("td");
+                row.appendChild(cell7);
+            }
 
 
 
-            
             document.getElementById("avengerBody").appendChild(row);
             //so the variable "row" we created alllll the way at the top of the for loop 
             //will be appended to our empty table body in the HTML
 
         }
-
-
-
-
-
-
 
     }
 
