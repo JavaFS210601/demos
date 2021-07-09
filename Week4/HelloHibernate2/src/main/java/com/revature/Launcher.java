33eb60b4addee0a66924877bb5add82453552e89
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-09 16:17:03 -0400

    FinishedHelloHibernate2

--- Week4/HelloHibernate2/src/main/java/com/revature/Launcher.java
+++ Week4/HelloHibernate2/src/main/java/com/revature/Launcher.java
@@ -2,12 +2,21 @@ package com.revature;
 
 import java.util.List;
 
+import com.revature.daos.AuthorDAO;
 import com.revature.daos.BookDAO;
 import com.revature.models.Author;
 import com.revature.models.Book;
 
+//Changes made since HelloHibernate1:
+	//Added a field in Author to reference a List of Books belonging to each author (This is effectively ManyToMany)
+		//Thus, we had to change the Author boilerplate code
+	//Created the AuthorDAO (same as the BookDAO but for Authors)
+	//Fixed the infinite loop by changing the toString() method in Book (Read there for how we did it)
+	//Experimented with the AuthorDAO in the Launcher
+
 public class Launcher {
 
+	private static AuthorDAO aDAO = new AuthorDAO();
 	private static BookDAO bDAO = new BookDAO(); //so we can use the BookDAO methods
 	//needs to be static so we can call it in our static main method
 	
@@ -41,6 +50,31 @@ public class Launcher {
 		//print out the List of books in an enhanced for loop
 		for(Book b : books) {
 			System.out.println(b);
+		}
+		
+		System.out.println("===============================================================(HelloHibernate2 Stuff");
+		
+		//Make a new Book object with an existing author as its author, and insert it using a DAO method
+		Book b4 = new Book("The Golem's Eye", "Fantasy", a1); //here, b4 is transient
+		bDAO.insertBook(b4); //now, it's persisted!
+		
+		//Make a new Author object and insert it using the DAO method
+		Author a4 = new Author("Bon", "Petruzziello", null); //here, a4 is transient
+		aDAO.insertAuthor(a4); //now, it's persisted
+		
+		//Populate a List of Authors using the DAO method
+		List<Author> authors = aDAO.findAllAuthors();
+		
+		//Populate a new List of Books to include b4, which we just added
+		List<Book> newBookList = bDAO.findAllBooks();
+		
+		//print out all the books, and all the authors using enhanced for loops to print em out
+		for(Book b : newBookList) {
+			System.out.println(b);
+		}
+		
+		for(Author a : authors) {
+			System.out.println(a);
 		}
 		
 	}
