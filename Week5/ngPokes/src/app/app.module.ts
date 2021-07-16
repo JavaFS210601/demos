88de6724fdecf7657968b5d70d27e91ed5007bc2
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-16 11:02:10 -0400

    chani thx

--- Week5/ngPokes/src/app/app.module.ts
+++ Week5/ngPokes/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; //we had to manually import this
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- Week5/ngPokes/src/app/services/pokemon.service.ts
+++ Week5/ngPokes/src/app/services/pokemon.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../models/pokemon';
 
 //Injectable decorator is inherently associated with our SERVICES
 //Since we expect to inject services into models/components to give them functionality
@@ -9,6 +12,19 @@ import { Injectable } from '@angular/core';
 //Remember, the Services are what give functionality to our models/components
 export class PokemonService {
 
-  //
-  constructor() { }
+  //We need to dependecy inject HttpClient in order to use it in an HTTP request
+  constructor(private http:HttpClient) {}
+
+  //this method is what will get our Pokemon data from the api, to turn into a Pokemon object
+  //Note - we use a GET request to get pokemon data, and return it as an Observable
+    //Note note - we use Observables to hold data from a server. Like a promise but better
+
+  getPokemonFromApi(id:number):Observable<Pokemon> { //this function returns an obsservable of type pokemon
+    return this.http.get("https://pokeapi.co/api/v2/pokemon/" + id + "/") as Observable<Pokemon>
+  }
+
+  //This^^^ is basically an easier, more powerful way to do fetch()
+  //We just have to make sure to inject this service into the constructors of components/models
+          //so we can actually use it
+
 }
