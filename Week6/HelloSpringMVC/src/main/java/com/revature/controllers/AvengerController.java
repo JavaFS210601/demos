8a4fbaf67bcde0f3dbe19a4508c3b891fabbb39d
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-20 16:54:32 -0400

    almost done with HelloSpringMVC

--- Week6/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
+++ Week6/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +20,7 @@ import com.revature.models.Avenger;
 @Controller //indicates that the class will handle HTTP requests from DispatcherServlet
 @CrossOrigin //This acts as a CORS filter, allows all requests in if given no attributes
 @RequestMapping(value="/avengers") //so now all requests ending in /avengers will be directed here
+@ResponseBody //Adds @ResponseBody to every method, which will make them all return JSON
 public class AvengerController {
 
 	private AvengerDAO dao; //this class has an AvengerDAO field (so it's a dependency!)
@@ -27,11 +32,27 @@ public class AvengerController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET) //Ensures that and GET requests to /avengers uses this method
-	@ResponseBody //This will parse any Java object into JSON to send back as the response
+	//@ResponseBody //This will parse any Java object into JSON to send back as the response
 	public List<Avenger> getAllAvenger() {
 		return Arrays.asList(dao.getAll()); //we're returning the array that gets returned, but as a list
 	}
 	
+	@GetMapping("/{id}") //GetMapping will specify that GET requests with this endpoint go here
+	//@PathVariable allows you to get the path variable you specified in the mapping
+	//Spring has an Class called ResponseEntity that lets us set things like the status code of our response
+	public ResponseEntity<Avenger> getOneAvenger(@PathVariable("id") int id) {
+		
+		Avenger a = dao.getById(id);
+		
+		if(a==null) {
+			//Returning a ResponseEntity with an empty body with a no content status code
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //build is for when we send nothing back
+		} 
+		
+		//If an Avenger is returned, sned it back in the body of the response, and 200 status code
+		return ResponseEntity.status(HttpStatus.OK).body(a); //body is for when you send something back
+		
+	}
 	
 	
 }
