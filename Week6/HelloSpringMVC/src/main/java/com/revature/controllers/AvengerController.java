8131e3c4854db078137a26a9186476a422ba3afc
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-07-21 10:54:11 -0400

    Finished HelloSpringMVC - we love @RestController

--- Week6/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
+++ Week6/HelloSpringMVC/src/main/java/com/revature/controllers/AvengerController.java
@@ -10,17 +10,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.daos.AvengerDAO;
 import com.revature.models.Avenger;
 
-@Controller //indicates that the class will handle HTTP requests from DispatcherServlet
+@RestController //This annotation does everything @Controller and @ResponseBody do. Convenient!
+//@Controller //indicates that the class will handle HTTP requests from DispatcherServlet
+//@ResponseBody //Adds @ResponseBody to every method, which will make them all return JSON
 @CrossOrigin //This acts as a CORS filter, allows all requests in if given no attributes
 @RequestMapping(value="/avengers") //so now all requests ending in /avengers will be directed here
-@ResponseBody //Adds @ResponseBody to every method, which will make them all return JSON
 public class AvengerController {
 
 	private AvengerDAO dao; //this class has an AvengerDAO field (so it's a dependency!)
@@ -53,6 +57,20 @@ public class AvengerController {
 		return ResponseEntity.status(HttpStatus.OK).body(a); //body is for when you send something back
 		
 	}
+	
+	@PutMapping //PutMapping will specify that PUT requests with this endpoint go here
+												//@RequestBody turns the incoming JSON into Java
+	public ResponseEntity<Avenger> updateAvenger(@RequestBody Avenger a){
+		a = dao.update(a);
+		if(a==null) {
+			//If an invalid Avenger is sent, return a ResponseEntity with an empty body with a no content status code
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //build is for when we send nothing back
+		} 
+		
+		//If a valid Avenger is sent, send it back in the body of the response, and 200 status code
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(a); //body is for when you send something back
+	}
+	
 	
 	
 }
