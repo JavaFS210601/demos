8acbe1a0863e833598d4d4e9df71d57f045fe019
Ben Petruzziello <benpetruzziello@MacBook-Air-7.fios-router.home>
2021-07-27 16:18:34 -0400

    3 methods in HelloSpringData

--- Week7/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
+++ Week7/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
@@ -1,10 +1,14 @@
 package com.revature.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +35,32 @@ public class PokemonController {
 	public ResponseEntity<List<Pokemon>> getAllPokemon() {
 		return ResponseEntity.status(200).body(pDAO.findAll());
 	}
+	
+	//This method will take an endpoint after /pokemon and look for any Pokemon with a name matching that endpoint
+	@GetMapping("/{name}")//the value of the URL endpoint will be the parameter in this method - hence @PathVariable
+	public ResponseEntity<List<Pokemon>> findByName(@PathVariable String name){
+		
+		//Remember, Optional is an object that MAY have whatever the generic is, or it may be null. Avoids NullPointerException
+		Optional<List<Pokemon>> opt = pDAO.findByName(name); //populate a List of Pokemon from the DAO method
+		
+		List<Pokemon> pokeList = null; //instantiate an empty List of Pokemon, to get filled if data is successfully returned.
+		
+		//control flow based on the value of our Optional "opt" variable
+		if(opt.isPresent()) { //if my optional exists...
+			pokeList = opt.get(); //get the contents of the Optional object and put it into our pokeList
+		}
+		
+		return ResponseEntity.status(200).body(pokeList); //the contents of pokeList will be sent as JSON (thanks to @RestController)
+		
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity addPokemon(@RequestBody Pokemon p) { //The body of the Post Request is JSON, @ResponseBody will make it a POJO
+		pDAO.save(p);
+		return ResponseEntity.status(201).build(); //.build() because we don't care to return any data (contrary to .body())
+		//201 = created - your resource was successfully created/persisted
+	}
+	
 	
 }
--- Week7/HelloSpringData/src/main/java/com/revature/daos/PokemonDAO.java
+++ Week7/HelloSpringData/src/main/java/com/revature/daos/PokemonDAO.java
@@ -1,5 +1,8 @@
 package com.revature.daos;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.revature.models.Pokemon;
@@ -8,6 +11,17 @@ import com.revature.models.Pokemon;
 //The second is the data type of the primary key
 public interface PokemonDAO extends JpaRepository<Pokemon, Integer> {
 
+	//I want a method that finds Pokemon by name, but that doesn't exist in JpaRepository... 
+	//Gotta define a method signature here! 
 	
+	//An Optional is an object that MAY have whatever the generic is, or it may be null. Avoids NullPointerExceptions!
+	public Optional<List<Pokemon>> findByName(String name); //for this method to work, the name must include the field name in camel case
+	
+	//We had to define this method signature on our own...
+	//BUT Spring Data will create this method body for us!
+	
+	
+	//will explore if this works...
+	//public Optional<List<Pokemon>> findByNameType(String name, String type);
 	
 }
